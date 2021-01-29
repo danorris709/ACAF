80a97687845076a08002aecc0e82ba0c1448900e
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-01-29 02:12:35 +0000

    Added CommandFactory registering and unregistering injectors

--- src/main/java/com/envyful/acaf/api/CommandFactory.java
+++ src/main/java/com/envyful/acaf/api/CommandFactory.java
@@ -2,10 +2,16 @@ package com.envyful.acaf.api;
 
 import com.envyful.acaf.api.exception.CommandLoadException;
 
+import java.util.function.Supplier;
+
 public interface CommandFactory {
 
     boolean registerCommand(Object o) throws CommandLoadException;
 
     boolean unregisterCommand(Object o);
+
+    void registerInjector(Class<?> parentClass, Supplier<?> supplier);
+
+    void unregisterInjector(Class<?> parentClass);
 
 }
