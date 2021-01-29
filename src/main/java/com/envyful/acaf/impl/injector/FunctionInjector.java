83365d765b0aea41d2f87743684fcdb7cc435add
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-01-29 02:53:56 +0000

    Implemented into FunctionInjector

--- src/main/java/com/envyful/acaf/impl/injector/FunctionInjector.java
+++ src/main/java/com/envyful/acaf/impl/injector/FunctionInjector.java
@@ -8,16 +8,23 @@ import java.util.function.BiFunction;
 public class FunctionInjector<T> implements ArgumentInjector<T> {
 
     private final Class<T> superClass;
+    private final boolean multipleArgs;
     private final BiFunction<ICommandSender, String[], T> function;
 
-    public FunctionInjector(Class<T> superClass, BiFunction<ICommandSender, String[], T> function) {
+    public FunctionInjector(Class<T> superClass, boolean multipleArgs, BiFunction<ICommandSender, String[], T> function) {
         this.superClass = superClass;
+        this.multipleArgs = multipleArgs;
         this.function = function;
     }
 
     @Override
     public Class<T> getSuperClass() {
         return this.superClass;
+    }
+
+    @Override
+    public boolean doesRequireMultipleArgs() {
+        return this.multipleArgs;
     }
 
     @Override
