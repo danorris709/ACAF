0925b049550aeaf29662d041f470359a2bcb95f6
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-01-29 02:01:00 +0000

    Changed to runtime exception

--- src/main/java/com/envyful/acaf/api/exception/CommandLoadException.java
+++ src/main/java/com/envyful/acaf/api/exception/CommandLoadException.java
@@ -1,6 +1,6 @@
 package com.envyful.acaf.api.exception;
 
-public class CommandLoadException extends Exception {
+public class CommandLoadException extends RuntimeException {
 
     public CommandLoadException(String className, String reason) {
         super("Failed to load command " + className + " for reason: " + reason);
