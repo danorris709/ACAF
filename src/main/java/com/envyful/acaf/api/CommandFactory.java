7adb6ba873cc88794b71fd85b55edfcd198d598d
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-01-29 01:59:28 +0000

    Added throws CommandLoadException to CommandFactory.java

--- src/main/java/com/envyful/acaf/api/CommandFactory.java
+++ src/main/java/com/envyful/acaf/api/CommandFactory.java
@@ -1,8 +1,10 @@
 package com.envyful.acaf.api;
 
+import com.envyful.acaf.api.exception.CommandLoadException;
+
 public interface CommandFactory {
 
-    boolean registerCommand(Object o);
+    boolean registerCommand(Object o) throws CommandLoadException;
 
     boolean unregisterCommand(Object o);
 
