e5150f4f041ca63b90f7e7cd22cda407fb78f32d
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-01-29 01:45:29 +0000

    Added Child annotation

---
+++ src/main/java/com/envyful/acaf/api/command/Child.java
@@ -0,0 +1,12 @@
+package com.envyful.acaf.api.command;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.reflect.Type;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Child {
+}
