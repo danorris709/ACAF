ec76f13041fd42e2471e5ef05358c9e86c7adbdd
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-01-29 02:31:33 +0000

    Updated ArgumentInjector interface

--- src/main/java/com/envyful/acaf/api/injector/ArgumentInjector.java
+++ src/main/java/com/envyful/acaf/api/injector/ArgumentInjector.java
@@ -1,9 +1,11 @@
 package com.envyful.acaf.api.injector;
 
+import net.minecraft.entity.player.EntityPlayerMP;
+
 public interface ArgumentInjector {
 
     Class<?> getSuperClass();
 
-    Object instantiateClass();
+    Object instantiateClass(EntityPlayerMP sender, String... arguments);
 
 }
