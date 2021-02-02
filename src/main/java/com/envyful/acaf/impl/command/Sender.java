8dc7bb72fb94eff198dbc84ed2cdeb5370f5ae76
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-02 00:01:32 +0000

    Added types to sender

--- src/main/java/com/envyful/acaf/impl/command/Sender.java
+++ src/main/java/com/envyful/acaf/impl/command/Sender.java
@@ -1,10 +1,33 @@
 package com.envyful.acaf.impl.command;
 
+import com.google.common.collect.Maps;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayer;
+
+import java.util.Map;
+
 public enum Sender {
 
-    CONSOLE(),
-    PLAYER(),
+    CONSOLE(ICommandSender.class),
+    PLAYER(EntityPlayer.class),
 
     ;
 
+    private static final Map<Class<?>, Sender> SENDERS = Maps.newHashMap();
+
+    static {
+        for (Sender value : values()) {
+            SENDERS.put(value.clazz, value);
+        }
+    }
+
+    private final Class<?> clazz;
+
+    Sender(Class<?> clazz) {
+        this.clazz = clazz;
+    }
+
+    public static Sender get(Class<?> clazz) {
+        return SENDERS.get(clazz);
+    }
 }
